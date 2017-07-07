5fdc9b72a07600c9bc676d3d2d81e2e6912f90d1
Tristan McNab <tristan.j.mcnab@gmail.com>
2017-07-07 17:30:50 -0400

    Views: updated meta tags with recommended minimum (@joshbuchea/HEAD)

--- views/.components/Page.jsx
+++ views/.components/Page.jsx
@@ -4,7 +4,9 @@ const React = require('react')
 module.exports = ({ children, title }) =>
     <html>
         <head>
-            <meta charSet="utf-8" />
+            <meta charSet='utf-8' />
+            <meta httpEquiv='x-ua-compatible' content='ie-edge' />
+            <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no' />
             <title>{title}</title>
             <link rel='stylesheet' href='/style.css' />
         </head>
